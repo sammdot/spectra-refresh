99769cd4b5fc254c2cd7a7ad67ef3c22512c1dc4
Sammi De Guzman <sammi@sammdot.ca>
2022-07-01 20:31:58 -0700

    Recolor board images and add stroke numbers

--- src/Derivation.tsx
+++ src/Derivation.tsx
@@ -1,8 +1,38 @@
 import React, { useEffect, useState } from "react"
 import { Derivation } from "./api"
+import "./Derivation.css"
 
 type Props = {
   derivation: Derivation | null
+}
+
+function recolorKeys(svg: string): string {
+  return svg
+    .replaceAll(/style="fill:#7F7F7F;.+?"/g, 'class="key"') // base
+    .replaceAll(/style="fill:#007FFF;.+?"/g, 'class="key matched"') // matched
+    .replaceAll(/style="fill:#DFDFDF;.+?"/g, 'class="key unmatched"') // unmatched
+    .replaceAll(/style="fill:#00AFFF;.+?"/g, 'class="key letters"') // letters
+    .replaceAll(/style="fill:#00AFAF;.+?"/g, 'class="key alt"') // alt
+    .replaceAll(/style="fill:#9FCFFF;.+?"/g, 'class="key rare"') // rare
+    .replaceAll(/style="fill:#8F8FFF;.+?"/g, 'class="key combo"') // combo
+    .replaceAll(/style="fill:#3F8F00;.+?"/g, 'class="key number"') // number
+    .replaceAll(/style="fill:#AFAF00;.+?"/g, 'class="key symbol"') // symbol
+    .replaceAll(/style="fill:#7FFFFF;.+?"/g, 'class="key spelling"') // spelling
+    .replaceAll(/style="fill:#FF7F00;.+?"/g, 'class="key brief"') // brief
+    .replaceAll(/style="fill:none;stroke:#000000;.+?"/g, 'class="chain-casing"')
+    .replaceAll(/style="fill:none;stroke:#B0B0B0;.+?"/g, 'class="chain-inner"')
+    .replaceAll(/style="fill:#000000;"/g, 'class="label"')
+}
+
+function addNumbers(svg: string): string {
+  let counter = 0
+  return svg.replaceAll(
+    /<use href=/g,
+    (match) =>
+      `<text class="counter" transform="translate(-1, 84)">${String.fromCharCode(
+        0x2460 + counter++
+      )}</text>${match}`
+  )
 }
 
 export default function DerivationDetail({ derivation }: Props) {
@@ -16,8 +46,9 @@ export default function DerivationDetail({ derivation }: Props) {
     }
   }, [derivation, pageNumber, setPageNumber, setHoverPageNumber])
 
+  const isBasePage = hoverPageNumber === 0
   const page = derivation
-    ? hoverPageNumber === 0
+    ? isBasePage
       ? derivation.basePage
       : derivation.pages[hoverPageNumber]
     : null
@@ -59,7 +90,7 @@ export default function DerivationDetail({ derivation }: Props) {
       <div
         className="board"
         dangerouslySetInnerHTML={{
-          __html: page.board,
+          __html: recolorKeys(isBasePage ? addNumbers(page.board) : page.board),
         }}
       />
     </>
